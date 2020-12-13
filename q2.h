20cc7def635f2a1efa3901f8d45fd49a178132a7
e-caste-mbp <en.cas.dev@icloud.com>
2020-12-13 13:33:17 +0100

    replace magic numbers in get_human_readable_time with constants. ref #15

--- q2.h
+++ q2.h
@@ -15,3 +15,13 @@
 #include <time.h>
 #include <string.h>
 #include <sys/resource.h>
+
+/*
+ * used in get_human_readable_time
+ * US = microseconds
+ * H = hour, M = minute, S = second, MS = millisecond
+*/
+#define US_IN_H 3600000000
+#define US_IN_M 60000000
+#define US_IN_S 1000000
+#define US_IN_MS 1000
