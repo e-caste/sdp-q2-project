7fd452f390520a465f829ebf9561fe413dc63dd1
e-caste-mbp <en.cas.dev@icloud.com>
2020-12-30 20:00:28 +0100

    add generate_graphs function to run.sh. ref #20

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 DATA_PATH="data"
 GRAIL_DATA_PATH="$DATA_PATH/grail-dags"
+GEN_SCRIPT_PATH="graph-generator-stq"
 GEN_DATA_PATH="$DATA_PATH/gen-dags"
 URL_PREFIX="https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/grail"
 DAGS=(
@@ -34,21 +35,6 @@ function print_usage {
   echo ""
 }
 
-function extract_downloaded_graphs {
-  echo "Extracting downloaded DAGs..."
-  for dag in "$GRAIL_DATA_PATH"/*.gz; do
-    [[ -e "$dag" ]] || break  # there are no .gz files
-    echo "Decompressing $dag with gzip..."
-    gzip --decompress "$dag"
-  done
-  for dag in "$GRAIL_DATA_PATH"/*.tar; do
-    [[ -e "$dag" ]] || break  # there are no .tar files
-    echo "Unarchiving $dag with tar..."
-    tar -xf "$dag" --directory "$GRAIL_DATA_PATH"
-    rm "$dag"
-  done
-}
-
 function download_graphs {
   # if the dir does not exist
   if [[ ! -d "$GRAIL_DATA_PATH" ]]; then
@@ -73,6 +59,47 @@ function download_graphs {
   fi
 }
 
+function extract_downloaded_graphs {
+  echo "Extracting downloaded DAGs..."
+  for dag in "$GRAIL_DATA_PATH"/*.gz; do
+    [[ -e "$dag" ]] || break  # there are no .gz files
+    echo "Decompressing $dag with gzip..."
+    gzip --decompress "$dag"
+  done
+  for dag in "$GRAIL_DATA_PATH"/*.tar; do
+    [[ -e "$dag" ]] || break  # there are no .tar files
+    echo "Unarchiving $dag with tar..."
+    tar -xf "$dag" --directory "$GRAIL_DATA_PATH"
+    rm "$dag"
+  done
+}
+
+function generate_graphs {
+  graphs=($(find "$GEN_SCRIPT_PATH" -maxdepth 1 -name "*.gra"))
+  # if there are no .gra files in the script directory
+  if [[ ${#graphs[@]} -eq 0 ]]; then
+    # see cd handling: https://github.com/koalaman/shellcheck/wiki/SC2103
+    cd "$GEN_SCRIPT_PATH" || exit
+    make
+    echo "Running $GEN_SCRIPT_PATH/run.sh, which contains the following calls:"
+    cat run.sh
+    ./run.sh
+    cd ..
+  else
+    echo "It seems you have already generated the benchmark graphs."
+    echo "Skipping DAG generation..."
+  fi
+  if [[ ! -d "$GEN_DATA_PATH" ]]; then
+    mkdir -p "$GEN_DATA_PATH"
+  fi
+  echo "Moving generated DAGs to $GEN_DATA_PATH..."
+  for dag in "$GEN_SCRIPT_PATH"/*.gra; do
+    echo -n "$dag" | cut -d / -f 2
+  done
+  mv "$GEN_SCRIPT_PATH"/*.gra "$GEN_DATA_PATH"
+  mv "$GEN_SCRIPT_PATH"/*.que "$GEN_DATA_PATH"
+}
+
 # no arguments given
 if [[ $# -eq 0 ]]; then
   print_usage
@@ -91,6 +118,10 @@ while [[ $# -gt 0 ]]; do
     -d|--download)
     download_graphs
     extract_downloaded_graphs
+    shift
+    ;;
+    -g|--generate)
+    generate_graphs
     shift
     ;;
     *)
