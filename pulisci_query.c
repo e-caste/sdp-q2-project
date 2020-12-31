65851fba52e3874cf7778e023baf58bf07935c84
e-caste-mbp <en.cas.dev@icloud.com>
2020-12-31 10:52:19 +0100

    print errors in english in pulisci_query.c. ref #32

--- pulisci_query.c
+++ pulisci_query.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main (int argc, char *argv[]) {
     if(argc != 2) {
-        printf("Inserire soltanto un argomento!!!\n");
+        fprintf(stderr, "Only one file argument expected, read %d instead.\n", argc);
         return 0;
     }
     FILE *fp;
     fp = fopen(argv[1], "r");
     if(fp == NULL) {
-        printf("Errore nell'aprire il file!!!\n");
+        fprintf(stderr, "Error opening file %s\n", argv[1]);
         return 0;
     }
 
