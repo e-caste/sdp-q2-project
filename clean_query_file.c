ea7813aa53bc55aa082ad4daf83ba8467ab0de40
e-caste-mbp <en.cas.dev@icloud.com>
2021-01-02 10:39:39 +0100

    replace static allocation with use of malloc in clean_query_file.c

--- clean_query_file.c
+++ clean_query_file.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main (int argc, char *argv[]) {
-    if(argc != 2) {
+
+    FILE *fp;
+    int num_query=0, **queries, i, a, b, c;
+
+    if (argc != 2) {
         fprintf(stderr, "Only one file argument expected, read %d instead.\n", argc);
         return 0;
     }
-    FILE *fp;
+
     fp = fopen(argv[1], "r");
-    if(fp == NULL) {
+    if (fp == NULL) {
         fprintf(stderr, "Error opening file %s\n", argv[1]);
         return 0;
     }
-
-    int num_query = 0;
 
     while (fscanf(fp, "%*[^\n]\n") != -1) {
         num_query++;
     }
 
-    int queries[num_query][2];
-    int i;
-    int a, b, c;
+    queries = (int **) malloc(num_query * sizeof(int *));
+    for (i=0; i<num_query; ++i) {
+        queries[i] = (int *) malloc(2 * sizeof(int));
+    }
 
     fseek(fp, 0L, SEEK_SET);
 
-    for (i=0; i<num_query; i++) {
+    for (i = 0; i < num_query; i++) {
         fscanf(fp, "%i %i %i\n", &a, &b, &c);
         queries[i][0] = a;
         queries[i][1] = b;
@@ -34,12 +38,11 @@ int main (int argc, char *argv[]) {
 
     fopen(argv[1], "w+");
 
-    for(i=0; i<num_query; i++) {
+    for (i = 0; i < num_query; i++) {
         fprintf(fp, "%i %i\n", queries[i][0], queries[i][1]);
     }
 
     fclose(fp);
 
     return 0;
-
 }
