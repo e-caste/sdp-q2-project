e612747e8a2717e107248f4a2dc6b4a413e51208
e-caste-mbp <en.cas.dev@icloud.com>
2021-01-19 21:36:04 +0100

    make use of logs directory in complete_benchmark.sh

--- complete_benchmark.sh
+++ complete_benchmark.sh
@@ -6,9 +6,9 @@
 [[ ! -d logs ]] && mkdir logs
 
 # only roots search times
-./run.sh --run --labels 0 | tee "log_$(date +%Y-%m-%d_%H-%M-%S)_mode=all_labels=0.txt"
+./run.sh --run --labels 0 | tee logs/"log_$(date +%Y-%m-%d_%H-%M-%S)_mode=all_labels=0.txt"
 
 for i in {1..10}; do
-  ./run.sh --run --labels "$i" benchmark | tee "log_$(date +%Y-%m-%d_%H-%M-%S)_mode=benchmark_labels=$i.txt"
-  ./run.sh --run --labels "$i" test | tee "log_$(date +%Y-%m-%d_%H-%M-%S)_mode=test_labels=$i.txt"
+  ./run.sh --run --labels "$i" benchmark | tee logs/"log_$(date +%Y-%m-%d_%H-%M-%S)_mode=benchmark_labels=$i.txt"
+  ./run.sh --run --labels "$i" test | tee logs/"log_$(date +%Y-%m-%d_%H-%M-%S)_mode=test_labels=$i.txt"
 done
