98913e9e92e26a83446e1f300eb91959c3c8dfa1
Enrico Alberti <enrico.alberti.18@gmail.com>
2021-02-07 00:12:29 +0100

    fix bug in refactor unsigned long; test on version 3 label parallelizzation

--- buildLabels.c
+++ buildLabels.c
@@ -103,10 +103,10 @@ void RandomizedLabelingParallelInit(row_g * graph, row_l * labels, int label_num
         args_lbl[i].threads_available = (num_threads/label_num);
 
         //MODIFIED VERSION: 3 THREAD FOR EACH LABELS + SOME SUB-THREADS SPLITTING ROOTS
-        //err_code = pthread_create(&threads_lbl[i], NULL, RandomizedLabelingRootsParallelInit, (void *)&args_lbl[i]);
+        err_code = pthread_create(&threads_lbl[i], NULL, RandomizedLabelingRootsParallelInit, (void *)&args_lbl[i]);
 
         //ORIGINAL VERSION: 1 THREAD FOR EACH LABELS
-        err_code = pthread_create(&threads_lbl[i], NULL, RandomizedLabelingParallel, (void *)&args_lbl[i]);
+        //err_code = pthread_create(&threads_lbl[i], NULL, RandomizedLabelingParallel, (void *)&args_lbl[i]);
         if(err_code) {
             printf ("RandomizedLabelingInit: Error number %i in creating thread %lu.\n", err_code, i);
             exit(1);
@@ -128,7 +128,8 @@ void RandomizedLabelingParallelInit(row_g * graph, row_l * labels, int label_num
 // RandomizedLabelingParallel is the PARALLEL version used for creating N Labels
 void* RandomizedLabelingParallel(void* args) {
     t_lbl_args* my_data;
-    my_data = (t_lbl_args*) args;    
+    my_data = (t_lbl_args*) args;
+    unsigned long j=0;
     unsigned long* indexes = (unsigned long *)malloc(my_data->roots_num*sizeof(unsigned long));
 
     // roots randomization (each thread its own)
@@ -137,7 +138,7 @@ void* RandomizedLabelingParallel(void* args) {
     memcpy(indexes, my_data->indexes, my_data->roots_num*sizeof(unsigned long));
     randomize(indexes, my_data->roots_num);
 
-    for(int j=0; j< my_data->roots_num; j++) {
+    for(j=0; j< my_data->roots_num; j++) {
         RandomizedVisitSequentialRecursive(my_data->roots[indexes[j]], my_data->lbl_num, my_data->labels, my_data->graph, &my_data->rank_node, my_data->vertex_num);
     }
 
@@ -158,7 +159,7 @@ void* RandomizedLabelingRootsParallelInit(void* args) {   //1 thread for each la
     // roots randomization (each thread its own)
     // roots are provided by the main
     // here we work in a shadow copy of original roots
-    memcpy(indexes, my_data->indexes, my_data->roots_num*sizeof(int));
+    memcpy(indexes, my_data->indexes, my_data->roots_num*sizeof(unsigned long)); //TODO modify in MASTER
     randomize(indexes, my_data->roots_num);
 
     if(my_data->threads_available > 1){//1 thread for each label + will run remaining thread splitting roots
@@ -229,55 +230,55 @@ void* RandomizedLabelingRootsParallel(void* args) { // some sub-threads splittin
 }
 
 void RandomizedVisitParallelRoots(unsigned long node_num, int lbl_num, row_l* labels, row_g* graph, unsigned long* rank_root, unsigned long num_vertex, pthread_mutex_t *rank_mutex){
-    unsigned long rank_children_min = num_vertex, i;
+    unsigned long rank_children_min = num_vertex, i, j;
     unsigned long children_num = graph[node_num].edge_num;
     unsigned long* indexes = (unsigned long *)malloc(children_num*sizeof(unsigned long));
 
     pthread_mutex_lock(graph[node_num].node_mutex);
     
-    if(labels[node_num].visited[lbl_num]) {
-        pthread_mutex_unlock(graph[node_num].node_mutex);
-        return;
-    }
+        if(labels[node_num].visited[lbl_num]) {
+            pthread_mutex_unlock(graph[node_num].node_mutex);
+            return;
+        }
 
-    //For each children of the node, recall the function
-    if(children_num > 0){
-        i=0;
+        //For each children of the node, recall the function
+        if(children_num > 0){
+            i=0;
+            
+            for(i=0; i<children_num; i++) {
+                indexes[i] = i;
+            }
+
+            randomize(indexes, children_num);
+
+            for(j=0; j<children_num; j++) {//for each children in random order
+                RandomizedVisitParallelRoots(graph[node_num].edges[indexes[j]], lbl_num, labels, graph, rank_root, num_vertex, rank_mutex);
+            }
+        }
         
-        for(i=0; i<children_num; i++) {
-            indexes[i] = i;
-        }
-
-        randomize(indexes, children_num);
-
-        for(int j=0; j<children_num; j++) {//for each children in random order
-            RandomizedVisitParallelRoots(graph[node_num].edges[indexes[j]], lbl_num, labels, graph, rank_root, num_vertex, rank_mutex);
-        }
-    }
-    
-    labels[node_num].visited[lbl_num] = true;
+        labels[node_num].visited[lbl_num] = true;
     pthread_mutex_unlock(graph[node_num].node_mutex);
 
-    pthread_mutex_lock(rank_mutex);
         //searching min value between children node
-        for(i=0; i<children_num; i++)
-            if(labels[graph[node_num].edges[i]].lbl_start[lbl_num] < rank_children_min)
-                rank_children_min = labels[graph[node_num].edges[i]].lbl_start[lbl_num];
-        tmp = *rank_root;
+        for(i=0; i<children_num; i++){
+            pthread_mutex_lock(graph[graph[node_num].edges[i]].node_mutex);
+                if(labels[graph[node_num].edges[i]].lbl_start[lbl_num] < rank_children_min)
+                    rank_children_min = labels[graph[node_num].edges[i]].lbl_start[lbl_num];
+            pthread_mutex_unlock(graph[graph[node_num].edges[i]].node_mutex);
+        }
+        
+    pthread_mutex_lock(rank_mutex);
+        if(*rank_root < rank_children_min)
+            labels[node_num].lbl_start[lbl_num] = *rank_root;
+        else 
+            labels[node_num].lbl_start[lbl_num] =  rank_children_min;
+        labels[node_num].lbl_end[lbl_num] = *rank_root;
+
         *rank_root = *rank_root + 1 ;
     pthread_mutex_unlock(rank_mutex);
 
-    pthread_mutex_lock(graph[node_num].node_mutex);
-        if(tmp < rank_children_min)
-            labels[node_num].lbl_start[lbl_num] = tmp;
-        else 
-            labels[node_num].lbl_start[lbl_num] =  rank_children_min;
-        labels[node_num].lbl_end[lbl_num] = tmp;
-    pthread_mutex_unlock(graph[node_num].node_mutex);
-
     if(indexes)
         free(indexes);
-
 }
 
 
